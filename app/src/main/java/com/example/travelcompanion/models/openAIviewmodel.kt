becb5e556a701b949354f92576f1fb9d0e0eb278
dwmccleery8 <dwmccleery8@gmail.com>
2023-12-12 16:11:31 -0500

    Updated to Epoch time!

--- app/src/main/java/com/example/travelcompanion/models/openAIviewmodel.kt
+++ app/src/main/java/com/example/travelcompanion/models/openAIviewmodel.kt
@@ -11,8 +11,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.travelcompanion.apis.OpenAiApi
 import kotlinx.coroutines.launch
+import java.time.Instant
 import java.time.LocalDate
 import java.time.Period
+import java.time.ZoneId
 
 sealed interface OpenAiState {
 
@@ -28,47 +30,43 @@ class OpenAiVM : ViewModel() {
     var tripType: String by mutableStateOf("Select type:")
     var isOutside: Boolean by mutableStateOf(false)
     var amountOfPeople: Int by mutableIntStateOf(0)
-    var returnDate: LocalDate? by mutableStateOf(null)
-    var departDate: LocalDate? by mutableStateOf(null)
     var otherUsefulInfo: String? by mutableStateOf(null)
-    var departHour: Int? by mutableStateOf(null)
-    var departMinute: Int? by mutableStateOf(null)
-    var arrivalHour: Int? by mutableStateOf(null)
-    var arrivalMinute: Int? by mutableStateOf(null)
-    var returnHour: Int? by mutableStateOf(null)
-    var returnMinute: Int? by mutableStateOf(null)
+    var departEpochTime: Instant? by mutableStateOf(null)
+    var arrivalEpochTime: Instant? by mutableStateOf(null)
+    var returnEpochTime: Instant? by mutableStateOf(null)
     var absoluteLow: Double? by mutableStateOf(null)
     var absoluteHigh: Double? by mutableStateOf(null)
     var chatGPTResponse: String? by mutableStateOf(null)
+    var departTimeSet: Boolean by mutableStateOf(false)
+    var returnTimeSet: Boolean by mutableStateOf(false)
     var responseReceived: Boolean by mutableStateOf(false)
 
     var openAiState: OpenAiState by mutableStateOf(OpenAiState.Loading)
         private set
 
+    val zoneId = ZoneId.of("EST")
+
     fun reset(){
         tripType = "Select type:"
         isOutside = false
         amountOfPeople = 0
-        returnDate = null
-        departDate = null
         otherUsefulInfo = null
-        departHour = null
-        departMinute = null
-        arrivalHour = null
-        arrivalMinute = null
-        returnHour = null
-        returnMinute = null
+        departEpochTime = null
+        arrivalEpochTime = null
+        returnEpochTime = null
         absoluteLow = null
         absoluteHigh = null
         chatGPTResponse = null
         responseReceived = false
+        departTimeSet = false
+        returnTimeSet = false
         openAiState = OpenAiState.Loading
     }
 
     fun readyToGo(): Boolean{
-        if ((tripType!=null)&&(amountOfPeople>0)&&(returnDate!=null)&&(departDate!=null)){
-            if (Period.between(departDate,returnDate).days<1){
-                return (departHour!=null)&&(departMinute!=null)&&(returnHour!=null)&&(returnMinute!=null)
+        if ((tripType!=null)&&(amountOfPeople>0)&&(returnEpochTime!=null)&&(departEpochTime!=null)){
+            if (Period.between(departEpochTime!!.atZone(zoneId).toLocalDate(),returnEpochTime!!.atZone(zoneId).toLocalDate()).days<1){
+                return (departTimeSet)&&(returnTimeSet)
             }
             else{
                 return true
@@ -93,7 +91,7 @@ class OpenAiVM : ViewModel() {
                         "Your task is to provide a brief, compressed list (in bullet point format with ONLY 10 bullet points) " +
                         "of travel essentials and items that are commonly forgotten on a $tripType road trip. " +
                         "The venue being traveled to is mainly $isOutsideText and there will be $amountOfPeople people there. " +
-                        "This person will be leaving on $departDate and will be returning home on $returnDate. " +
+                        "This person will be leaving on ${departEpochTime!!.atZone(zoneId).toLocalDate()} and will be returning home on ${returnEpochTime!!.atZone(zoneId).toLocalDate()}. " +
                         "This road trip will take 5 hours and it will be 32 degrees outside and sunny on arrival " +
                         "at the venue and 42 degrees outside and raining when the user returns. Other useful " +
                         "information about this trip inputted by the user includes:  \"$otherUsefulInfo\"."
